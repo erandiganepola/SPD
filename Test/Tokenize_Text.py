048fdf43b0403aaf38e61e6c7b5b83cfcb31c5b2
erandiganepola <erandiganepola@gmail.com>
2017-11-22 13:23:36 +0530

    basic pre-processing steps

--- Test/Tokenize_Text.py
+++ Test/Tokenize_Text.py
@@ -2,6 +2,8 @@ import codecs
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk import ngrams
+# -*- coding: utf8 -*-
 
 file_output_tokenize = codecs.open('tokens.txt', 'w', 'utf-16')
 file_output_punctuation = codecs.open('punctuation.txt', 'w', 'utf-16')
@@ -20,6 +22,12 @@ with codecs.open('sample_paragraph.txt', 'r', 'utf-16') as sampleFile:
         tokenArray.append(i)
     file_output_tokenize.close()
 
+## number replacement
+    # Iterate through the string, replacing "0" for the digits
+    for i in data:
+        if i.isdigit():
+            data = data.replace(i, "0")
+
 ## remove punctuation
     with codecs.open('tokens.txt', 'r', 'utf-16') as sampleFile:
         data = sampleFile.read()
@@ -28,11 +36,6 @@ with codecs.open('sample_paragraph.txt', 'r', 'utf-16') as sampleFile:
     file_output_punctuation.write(data)
 file_output_punctuation.close()
 
-## number replacement
-# Iterate through the string, replacing "0" for the digits
-for i in data:
-    if i.isdigit():
-        data = data.replace(i, "0")
 
 ## stop words removal
 stopWords = set(stopwords.words("sinhala"))
@@ -49,3 +52,21 @@ for i in tokenArray:
 print ('{} {}'.format('token length after removing stop words :', len(filtered_sentence)))
 file_output_stopwords.close()
 
+## create n-grams
+ngramArray = []
+n =3
+with codecs.open('tokens.txt', 'r', 'utf-16') as sampleFile:
+       data = sampleFile.read()
+       trigrams = ngrams(data.split(), n)
+       for grams in trigrams:
+          ngramArray.append(grams)
+       print ('{} {}'.format('ngrams are :', ngramArray))
+
+## synonym replacement
+f = codecs.open('sample_synonyms.txt', 'r', 'utf_8')
+x = f.read()
+y = str(x).splitlines()
+for i in y:
+    string = str(i)
+    words = string.split(',')
+print words
